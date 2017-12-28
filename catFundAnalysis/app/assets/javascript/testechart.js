621e12a103b9ea6548ae9fecee6c9f0fb292242b
yingxin li <yli120@ace.cs.fiu.edu>
2017-12-28 13:49:00 -0500

    add florida map

--- catFundAnalysis/app/assets/javascript/testechart.js
+++ catFundAnalysis/app/assets/javascript/testechart.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 import echarts from 'echarts';
 
 
@@ -23,3 +24,70 @@ myChart.setOption({
         data: [5, 20, 36, 10, 10, 20]
     }]
 });
+
+var countyChart = echarts.init(document.getElementById('county'));
+
+$.get('/static/assets/map/county.geojson', function (usaJson) {
+    countyChart.hideLoading();
+
+    echarts.registerMap('USA', usaJson, {
+    });
+    var option = {
+        title: {
+            text: 'Florida Hurrican Losses (2017)',
+            subtext: '',
+            sublink: '',
+            left: 'right'
+        },
+        tooltip: {
+            trigger: 'item',
+            showDelay: 0,
+            transitionDuration: 0.2,
+            formatter: function (params) {
+                var value = (params.value + '').split('.');
+                value = value[0].replace(/(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,');
+                return params.seriesName + '<br/>' + params.name + ': ' + value;
+            }
+        },
+        visualMap: {
+            left: 'right',
+            min: 500000,
+            max: 38000000,
+            inRange: {
+                color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
+            },
+            text:['High','Low'],           // 文本，默认为数值文本
+            calculable: true
+        },
+        toolbox: {
+            show: false,
+            //orient: 'vertical',
+            left: 'left',
+            top: 'top',
+            feature: {
+                dataView: {readOnly: false},
+                restore: {},
+                saveAsImage: {}
+            }
+        },
+        series: [
+            {
+                name: 'USA PopEstimates',
+                type: 'map',
+                roam: true,
+                map: 'USA',
+                itemStyle:{
+                    emphasis:{label:{show:true}}
+                },
+                // 文本位置修正
+                textFixed: {
+                },
+                data:[
+                ]
+            }
+        ]
+    };
+
+    countyChart.setOption(option);
+});
+                    
